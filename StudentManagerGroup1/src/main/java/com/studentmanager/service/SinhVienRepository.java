83f68304980fef5eb09b9caae74e726be6e25bb3
tieptv6669 <tieptv6669@gmail.com>
2018-10-26 11:58:48 +0700

    Tiep commit

--- StudentManagerGroup1/src/main/java/com/studentmanager/service/SinhVienRepository.java
+++ StudentManagerGroup1/src/main/java/com/studentmanager/service/SinhVienRepository.java
@@ -14,11 +14,7 @@ import com.studentmanager.entity.SinhVien;
 
 @Repository
 public interface SinhVienRepository extends JpaRepository<SinhVien, String> {
-	@Transactional
-	@Modifying
-	@Query("UPDATE SinhVien c SET c.TenSV = :tenSV,c.GioiTinh = :gioiTinh,c.NgaySinh = :ngaySinh,c.MaLop = :maLop,c.QueQuan = :queQuan  WHERE c.MaSV = :maSV")
-	void updateSinhVien(@Param("maSV") String maSV, @Param("tenSV") String tenSV, @Param("gioiTinh") boolean gioiTinh,
-			@Param("ngaySinh") Date ngaySinh, @Param("queQuan") String queQuan, @Param("maLop") String maLop);
+	
 }
 	
 
