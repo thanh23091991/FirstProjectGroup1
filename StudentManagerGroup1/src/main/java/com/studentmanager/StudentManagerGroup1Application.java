1cb791a980cb90186a12f45f5a9db0adfbb579a7
Pham Ngoc Thanh <thanhhvnh91@gmail.com>
2018-10-26 11:23:01 +0700

    Fist Commit PNT

---
+++ StudentManagerGroup1/src/main/java/com/studentmanager/StudentManagerGroup1Application.java
@@ -0,0 +1,12 @@
+package com.studentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagerGroup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagerGroup1Application.class, args);
+	}
+}
