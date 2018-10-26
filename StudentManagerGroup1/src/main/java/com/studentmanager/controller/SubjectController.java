a36859bed95d73c514a2831e3dcafc8b7945c010
Vuong Quang Dong 1612 <quangdong1612@gmail.com>
2018-10-26 17:32:05 +0700

    Dong commit

--- StudentManagerGroup1/src/main/java/com/studentmanager/controller/SubjectController.java
+++ StudentManagerGroup1/src/main/java/com/studentmanager/controller/SubjectController.java
@@ -1,5 +1,82 @@
 package com.studentmanager.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.studentmanager.entity.MonHoc;
+import com.studentmanager.service.DiemRepository;
+import com.studentmanager.service.MonHocRepository;
+
+@Controller
 public class SubjectController {
+	@Autowired
+	MonHocRepository monhocRepository;
+
+	@Autowired
+	DiemRepository diemRepository;
+
+	@RequestMapping("/subject-show")
+	public String showMonHoc(Model model) {
+		model.addAttribute("subjectlist", monhocRepository.findAll());
+		return "TableSubjectResult";
+	}
+
+	@RequestMapping(value="/subjectDelete/{id}")
+	public String doDeleteMonHoc(@PathVariable String id, Model model) {
+
+		if (diemRepository.findByMaMH(id).size() == 0) {
+			monhocRepository.deleteById(id);
+			model.addAttribute("subjectlist", monhocRepository.findAll());
+			return "redirect:/subject-show";
+		} else {
+			System.out.println(diemRepository.findByMaMH(id).size());
+			model.addAttribute("deletedMsg", "Bạn cần xoá thông tin trong bảng Điểm");
+			return "redirect:/subject-show";
+		}
+	}
+
+	@RequestMapping(value = "/insSubInfo", method = RequestMethod.GET)
+	public String insertMonHocInfo() {
+		return "InsertSubject";
+	}
+
+	@RequestMapping(value = "insertSubject", method = RequestMethod.POST)
+	public String doInsertMonHoc(@ModelAttribute("monhoc") MonHoc MonHoc, Model model) {
+		boolean check = false;
+		String s = null;
+		if (MonHoc.getTenMH().isEmpty() || MonHoc.getTenMH() == null) {
+			check = true;
+			model.addAttribute("mes_error", "Vui lòng điền đầy đủ thông tin!!!!");
+		}
+		if (MonHoc.getMaMH().isEmpty() || MonHoc.getMaMH() == null) {
+			check = true;
+			model.addAttribute("mes_error", "Vui lòng điền đầy đủ thông tin!!!!");
+
+		}
+		if (MonHoc.getSoTrinh() < 0 || MonHoc.getSoTrinh() > 7) {
+			check = true;
+			model.addAttribute("mes_error", "Số trình phải lớn hơn 0 và nhỏ hơn 7!!!!");
+		}
+		if (check) {
+			return "InsertSubject";
+		} else
+			monhocRepository.save(MonHoc);
+		return "redirect:/subject-show";
+	}
+	
+	@RequestMapping(value = "/updateSubInfo/{id}")
+	public String updateMonHocInfo(@PathVariable String id,Model model) {
+		Optional<MonHoc> monhoc = monhocRepository.findById(id);
+		model.addAttribute("monhoc", monhoc.get());
+		return "UpdateSubject";
+	}
 
 }
