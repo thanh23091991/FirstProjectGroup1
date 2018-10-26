e2f48429c2c17d6587bfd078a8829e01c5254a8e
tieptv6669 <tieptv6669@gmail.com>
2018-10-26 17:15:12 +0700

    Tiep commit

--- StudentManagerGroup1/src/main/java/com/studentmanager/controller/HomeController.java
+++ StudentManagerGroup1/src/main/java/com/studentmanager/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.studentmanager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class HomeController {
 	@RequestMapping("/")
